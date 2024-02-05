b091b96ef3a3b6b178c36e6e9ae7829ea120d72f
Anderson Louzada <anderson.s.louzada@gmail.com>
2024-02-05 11:39:49 -0300

    feat: adds limitation of access

--- src/main/java/com/betrybe/agrix/advice/GeneralControllerAdvice.java
+++ src/main/java/com/betrybe/agrix/advice/GeneralControllerAdvice.java
@@ -7,6 +7,7 @@ import com.betrybe.agrix.exception.PersonExistsException;
 import com.betrybe.agrix.exception.PersonNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -56,6 +57,11 @@ public class GeneralControllerAdvice {
   @ExceptionHandler(BadCredentialsException.class)
   public ResponseEntity<String> handleBadCredentialsException(BadCredentialsException e) {
     return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Credenciais inválidas!");
+  }
+
+  @ExceptionHandler(AccessDeniedException.class)
+  public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException e) {
+    return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado!");
   }
 
 }
